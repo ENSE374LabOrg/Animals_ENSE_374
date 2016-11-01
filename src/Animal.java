7b61c3ac9b398d773e3604726997d1981fc85a5c
Ahmed Shamiss <a_shamiss@hotmail.com>
2016-11-01 15:56:11 -0600

    Test

---
+++ src/Animal.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author shamissa
+ *
+ */
+public class Animal {
+	private String name;
+	
+	public Animal(){
+		name = "";
+	}
+	
+	public void eat(){
+		System.out.println("Animal eats food");
+	}
+	
+	public void death(){
+		System.out.println("Animal died of hunger.");
+	}
+}
